7d2ecad924d84678117a004020c3f1016541c70b
Jeff <84194418+JeffNawroth@users.noreply.github.com>
2022-01-18 17:06:42 +0100

    + added Toppings

--- ARMenu/Models/ModelData.swift
+++ ARMenu/Models/ModelData.swift
@@ -15,7 +15,7 @@ var product3:Product = Product(image: Image("eiscafe"), name: "Eiscafé", catego
 class ModelData: ObservableObject{
     
     @Published var products: [Product] =  [
-        Product(image: Image("kaesekuchen"), name: "Käsekuchen", category: "Kuchen", price: 3.50, description: "Suscipit inceptos est felis purus aenean aliquet adipiscing diam venenatis, augue nibh duis neque aliquam tellus condimentum sagittis vivamus.",  isVegan: true,isBio: true, isFairtrade: true, nutritionFacts: NutritionFacts(calories: 573, fat: 23.2, carbs: 44.9, protein: 3.4),allergens: Product.dummyAllergens, additives: Product.dummyAdditives),
+        Product(image: Image("kaesekuchen"), name: "Käsekuchen", category: "Kuchen", price: 3.50, description: "Suscipit inceptos est felis purus aenean aliquet adipiscing diam venenatis, augue nibh duis neque aliquam tellus condimentum sagittis vivamus.",  isVegan: true,isBio: true, isFairtrade: true, nutritionFacts: NutritionFacts(calories: 573, fat: 23.2, carbs: 44.9, protein: 3.4),allergens: Product.dummyAllergens, additives: Product.dummyAdditives, toppings: Product.dummyToppings),
         
         Product(image: Image("spaghettieis"),name: "Spaghettieis", category: "Eis", price: 6.20, description: "Suscipit inceptos est felis purus aenean aliquet adipiscing diam venenatis, augue nibh duis neque aliquam tellus condimentum sagittis vivamus.", isVegan: true, isBio: false, isFairtrade: false, nutritionFacts: NutritionFacts(calories: 573, fat: 23.2, carbs: 44.9, protein: 3.4),allergens: Product.dummyAllergens, additives: Product.dummyAdditives),
         
