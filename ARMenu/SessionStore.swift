044868eb9a6a01deedefd3990c6b464b9ee539ed
ErenC61 <ec_61@hotmail.de>
2022-02-26 16:57:37 +0100

    Save commit

--- ARMenu/SessionStore.swift
+++ ARMenu/SessionStore.swift
@@ -14,45 +14,14 @@ class SessionStore: ObservableObject{
     var didChange = PassthroughSubject<SessionStore, Never>()
     @Published var loggedInUser: User? {didSet {self.didChange.send(self) }}
     var handle: AuthStateDidChangeListenerHandle?
-//    @Published var user: User = .init(restaurant: "", uid: "", email: "")
-//    @Published var guest: Bool = false
-//    @Published var loggedInUser = User()
     
     
     func listen(){
         handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
             if let user = user{
                 if !user.isAnonymous{
-//                    self.loggedInUser.uid
-//                    self.sess.uid = user.uid
-//                    self.loggedInUser.email = user.email
-//                    self.loggedInUser.userRole = .admin
                     self.loggedInUser = User(uid: user.uid, email: user.email, userRole: .admin)
                 }
-                
-                
-//                else{
-////                    self.session = User(restaurant: "",uid: user.uid, email: user.email!)
-//                    let db = Firestore.firestore()
-//                    db.collection("User").document(user.uid).getDocument { doc, error in
-//                        if error != nil{
-//                            print("Error: Fehler beim empfangen des Benutzers!")
-////                        } else{
-//    //                        let data = doc?.data()
-//    //                        print("Der Benutzer wurde erfolgreich empfangen!")
-//    ////                        self.session = User(data: data!)!
-//    //                        self.session = User(restaurant: "",uid: user.uid, email: user.email!)
-//    ////                        self.user = User(data: data!)!
-//    //                        print(self.session!.email)
-//    //                        print(self.session!.uid)
-//    //                        print(self.session!.restaurant)
-//    //                        return
-//                        }
-//
-//                    }
-//
-////
-//                }
             } else {
                 self.loggedInUser = nil
             }
@@ -72,25 +41,6 @@ class SessionStore: ObservableObject{
     func signUp(email: String, password: String, handler: @escaping AuthDataResultCallback){
         Auth.auth().createUser(withEmail: email, password: password, completion: handler)
     }
-    
-//    func createUserInfo(restaurant: String){
-//        let db = Firestore.firestore()
-//        let email = Auth.auth().currentUser?.email
-//        let userID = Auth.auth().currentUser?.uid
-//        let data = ["restaurant": restaurant, "email": email, "userID": userID]
-//
-//        user = User(restaurant: restaurant, uid: userID!, email: email!)
-//
-////        db.collection(restaurant).document("User").collection("Admin").document("\(userID!)").setData(["restaurant": restaurant ,"userEmail": email!, "userID": userID!])
-//        db.collection("User").document("\(userID!)").setData(data as [String : Any], merge: true) { error in
-//            if error != nil{
-//                print("Error: Fehler beim Erstellen des Benutzers!")
-//            } else{
-//                print("Benutzer wurde erfolgreich zur Datenbank hinzugefügt")
-//                self.getUser(uid: userID!)
-//            }
-//        }
-//    }
     
     func signIn(email: String, password: String, handler: @escaping AuthDataResultCallback){
         Auth.auth().signIn(withEmail: email, password: password, completion: handler)
@@ -145,23 +95,6 @@ class SessionStore: ObservableObject{
             }
         })
     }
-    
-//    func getUser(uid: String){
-//        let db = Firestore.firestore()
-//        db.collection("User").document(uid).getDocument { doc, error in
-//            if error != nil{
-//                print("Error: Fehler beim empfangen des Benutzers!")
-//            } else{
-//                let data = doc?.data()
-//                print("Der Benutzer wurde erfolgreich empfangen!")
-//                self.session = User(data: data!)!
-//                print(self.session!.email)
-//                print(self.session!.uid)
-//                print(self.session!.restaurant)
-//                return
-//            }
-//        }
-//    }
     
     deinit{
         unbind()
