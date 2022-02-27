ec76897b4f6549a7e412135ee1ad2e117601f2cd
Jeff <84194418+JeffNawroth@users.noreply.github.com>
2022-02-27 14:35:23 +0100

    # hopefully fixed bug with fetching data from loggedInUser

--- ARMenu/AppDelegate.swift
+++ ARMenu/AppDelegate.swift
@@ -20,7 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Create the SwiftUI view that provides the window contents.
         FirebaseApp.configure()
        let contentView = ContentView()
-            .environmentObject(ModelData())
             .environmentObject(SessionStore())
 
        // let contentView = TestView()
